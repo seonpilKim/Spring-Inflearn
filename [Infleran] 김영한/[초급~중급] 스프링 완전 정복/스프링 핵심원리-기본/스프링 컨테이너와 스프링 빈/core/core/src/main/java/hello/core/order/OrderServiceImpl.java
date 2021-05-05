80f61e5c6b475abe1db011e8486caf96c9aa56fe
seonpilKim <kimsunpil123@naver.com>
2021-05-05 23:56:54 +0900

    스프링 컨테이너와 스프링 빈

---
+++ [Infleran] 김영한/[초급~중급] 스프링 완전 정복/스프링 핵심원리-기본/스프링 컨테이너와 스프링 빈/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,36 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements  OrderService{
+
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // DIP 적용
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // 의존관계 주입(DI)
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        // 할인 정책에 관한 내용은 여기서 알 수 없음.
+        // 할인 정책에 관해 변경이 필요하면 discountPolicy만 수정하면 된다.
+        // 즉, 단일 책임 원칙(SRP)를 잘 지킨 경우이다.
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
