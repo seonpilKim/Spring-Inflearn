d9ad82433b85b63e4f3d04ad0f0a364280213988
seonpilKim <kimsunpil123@naver.com>
2021-05-10 03:07:18 +0900

    의존관계 자동 주입

---
+++ [Infleran] 김영한/[초급~중급] 스프링 완전 정복/스프링 핵심원리-기본/의존관계 자동 주입/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,34 @@
+package hello.core.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberServiceImpl implements MemberService{
+
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // DIP 적용
+    private final MemberRepository memberRepository;
+
+    // 의존관계 주입(DI)
+    @Autowired
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
+}
